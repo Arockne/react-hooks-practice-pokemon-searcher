8442faf1a894610b76b7674c43990dfdba454848
Daniel Calvin <arockne2003@yahoo.com>
2021-09-06 12:44:38 -0700

    Implement search to output partial match

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -1,10 +1,22 @@
 import React, { useState } from 'react'
 
-function SearchInput() {
+function SearchInput({ onSearch }) {
+  const [prompt, setPrompt] = useState('')
+
+  function handleInputChange(event) {
+    setPrompt(event.target.value)
+  }
+
+  function handleFormSubmit(event) {
+    event.preventDefault()
+    onSearch(prompt)
+    setPrompt('')
+  }
+
   return (
-    <form>
+    <form onSubmit={handleFormSubmit}>
       <div className="ui icon input">
-        <input className="prompt" />
+        <input className="prompt" value={prompt} onChange={handleInputChange}/>
         <i className="search icon" />
       </div>
     </form>
